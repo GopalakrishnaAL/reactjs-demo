2eb387172af20dc1d7e6e835bf4f1611351b4897
gopalakrishnaal <gopalakkrishna.al@gmail.com>
2023-12-11 13:04:51 +0000

    intial commit

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,25 @@
 # Deploying the container using docker-compose
 docker-compose up -d
 
+# Set the Docker repository based on the branch
+if [ "$BRANCH_NAME" == "dev" ]; then
+    DOCKER_REPO="gopalakrishnaal/project:dev"
+elif [ "$BRANCH_NAME" == "master" ]; then
+    DOCKER_REPO="gopalakrishnaal/project:prod"
+else
+    echo "Unsupported branch: $BRANCH_NAME"
+    exit 1
+fi
+
 # Tagging the image
-docker tag project:web gopalakrishnaal/project:cicd
+docker tag project $DOCKER_REPO
 
 # Pushing the image to Docker Hub
-docker push gopalakrishnaal/project:cicd
+docker push $DOCKER_REPO
 
 # Check if the push was successful
 if [ $? -eq 0 ]; then
-  echo "The image has been successfully pushed to Docker Hub."
+    echo "The image has been successfully pushed to Docker Hub."
 else
-  echo "Failed to push the image to Docker Hub."
+    echo "Failed to push the image to Docker Hub."
 fi
